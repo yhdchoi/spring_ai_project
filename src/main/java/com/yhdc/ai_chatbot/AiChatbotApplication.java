461359202387510ecf6c81ecf46c5d4c30c0b910
yhdchoi <yhdchoi@gmail.com>
2025-03-25 14:31:35 +0900

    Initial Commit

---
+++ src/main/java/com/yhdc/ai_chatbot/AiChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.yhdc.ai_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhdc/ai_chatbot/AiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhdc.ai_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
